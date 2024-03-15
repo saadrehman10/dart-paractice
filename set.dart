90fc601eb8c61848929e0eef1b7ff397553c8b23
Saad ur Rehman <125679304+saadrehman10@users.noreply.github.com>
2024-03-15 17:10:10 +0500

    commit

--- set.dart
+++ set.dart
@@ -18,7 +18,26 @@ void main() {
   print(list.isEmpty);
   list.remove(1);
   print(list);
-  list.clear();
-  print(list);
+  Set<int> array = {
+    1,
+    2,
+    4,
+    5,
+    6,
+    777,
+    45,
+    3,
+    67753,
+  };
+  print(array.elementAt(0));
+  print(array.elementAtOrNull(33));
+  var set = list.difference(array);
+  print(set);
+  var intersection = array.intersection(list);
+  print(intersection);
+  // List<int> hello = [1, 2, 4, 5, 6];
+
+  var setType = {1, '123', () => 2, true, null, if (true) 3 else 4,'hello'};
   
+  print(setType);
 }
