ada1a49cfa0648cd57e7d7acd0794d983a907110
Saad ur Rehman <125679304+saadrehman10@users.noreply.github.com>
2024-03-18 20:55:55 +0500

    update

--- todoList.dart
+++ todoList.dart
@@ -1,50 +1,62 @@
 import 'dart:io';
 
+dynamic userInput(type, [String? textDisplayed]) {
+  textDisplayed != null ? print(textDisplayed) : false;
+  dynamic input;
+  try {
+    input =
+        type == "I" ? int.parse(stdin.readLineSync()!) : stdin.readLineSync()!;
+    print('\n');
+  } catch (e) {
+    print('Invalid input, Try Again\n');
+    return userInput(type);
+  }
+  return input;
+}
+
+void printList(Map todolist) {
+  var keys = todolist.keys.toList();
+  print("The Current :\n");
+  for (var i in keys) {
+    print("$i. ${todolist[i]}");
+  }
+  print('\n');
+}
+
 void main() {
-  Map<int, dynamic> todolist = {};
+  Map<int, dynamic> todolist = {
+    1: "buy grocery",
+    2: "clean the house",
+    3: "go to gym"
+  };
   print('Welcome to Todo list\n');
   bool loop = true;
   while (loop) {
-    print(
-        "Select an option\n1. Add a task\n2. Delete a task\n3. Show all tasks\n4. Quit");
-    int? opt;
-    try {
-      int optSelected = int.parse(stdin.readLineSync()!);
-      opt = optSelected;
-    } catch (e) {
-      print("Invalid input!\nPlease enter a number.");
-    }
-    ;
+    dynamic opt = userInput("I",
+        "Select an option:\n\n1. Add a task\n2. Delete a task\n3. Show all tasks\n4. Quit\n\nEnter the Selected option: ");
     switch (opt) {
       case 1:
-        try {
-          String task = stdin.readLineSync()!;
-          todolist[todolist.length + 1] = task;
-        } catch (e) {
-          print('Please enter the correct value' + '\n' + e.toString());
-        }
+        String task = userInput("s", 'Enter the task:\n\t');
+        todolist[todolist.length + 1] = task;
       case 2:
         print('Which task you wanted to delete ?\n');
-        var keys = todolist.keys.toList();
-        for (var i in keys) {
-          print("$i. ${todolist[i]}");
-        }
-        print('\n');
-        int? opt = int.parse(stdin.readLineSync()!);
-        if (!todolist.containsKey(opt)|| opt != int) throw Exception('Invalid Option');
-        todolist.remove(opt);
-        var value = todolist.values.toList();
-        todolist.clear();
-        for (int i = 0; i < value.length; i++) {
-          todolist[i + 1] = value[i];
+        printList(todolist);
+        int delOpt = userInput("I", 'Enter the Selected Option');
+        if (todolist.containsKey(delOpt)) {
+          todolist.remove(delOpt);
+          var value = todolist.values.toList();
+          todolist.clear();
+          for (int i = 0; i < value.length; i++) {
+            todolist[i + 1] = value[i];
+          }
+        } else {
+          print('Task Option dosn\'t exist,\nTry again');
+          opt != null ? opt = 2 : false;
         }
       case 3:
-        var keys = todolist.keys.toList();
-        for (var i in keys) {
-          print("$i. ${todolist[i]}");
-        }
+        printList(todolist);
       default:
-        opt != 4 ? print('selected correct option') : loop = false;
+        opt != 4 ? false : loop = false;
     }
   }
 }
