d8b2fa3fb2c345f12de2a606bcfae54f8280895c
Saad ur Rehman <125679304+saadrehman10@users.noreply.github.com>
2024-03-19 02:05:30 +0500

    update

---
+++ fileHandle.dart
@@ -0,0 +1,33 @@
+import 'dart:io';
+
+void main() {
+  // File file = File('test.txt');
+  // String contents = file.readAsStringSync();
+  // print(contents);
+  // print(file.path);
+  // print(file.absolute.path);
+  // print(file.lengthSync());
+  // print(file.lastModifiedSync());
+  // print(file.lastAccessedSync());
+  // File file1 = File('test.csv');
+  // String cont = file1.readAsStringSync();
+  // var list = cont.split('\n');
+  // for (var i in list) {
+  //   print(i);
+  // }
+  // file.writeAsStringSync('hello');
+  // contents = file.readAsStringSync();
+  // print(contents);
+  // file.writeAsStringSync('This is teh apppended part',mode: FileMode.append);
+  // contents = file.readAsStringSync();
+  // print(contents);
+  File file = File('student.csv');
+  while (true) {
+    int? opt = int.parse(stdin.readLineSync()!);
+    switch (opt) {
+      case 1 :
+        try{
+          
+    }
+  }
+}
