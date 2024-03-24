9f75686c86b1386fc248cc1431185feaedf60d10
Saad ur Rehman <125679304+saadrehman10@users.noreply.github.com>
2024-03-25 01:20:36 +0500

    update

--- opp i dart/class.dart
+++ opp i dart/class.dart
@@ -16,16 +16,32 @@ class PrintRandon {
   }
 }
 
+class Camera {
+  String? brandName;
+  String? color;
+  String? megapixel;
+  String? display;
+  String printDetail() {
+    return 'The brand of this camera is $brandName\nThe color of the camera is $color\nThe camera has a $megapixel senser\nAnd has a $display inches diplay';
+  }
+}
+
 void main() {
-  Person saad = Person();
-  saad.name = 'Saad ur Rehman';
-  saad.age = 22;
-  saad.address = 'Pakistan';
-  saad.height = 6.1;
-  saad.printPersonDetail();
-  saad.phoneNo = 923132274337;
-  saad.printPersonDetail();
-  Person Ali = Person();
-  Ali.name = 'Ali Khan';
-  Ali.printPersonDetail();
+  // Person saad = Person();
+  // saad.name = 'Saad ur Rehman';
+  // saad.age = 22;
+  // saad.address = 'Pakistan';
+  // saad.height = 6.1;
+  // saad.printPersonDetail();
+  // saad.phoneNo = 923132274337;
+  // saad.printPersonDetail();
+  // Person Ali = Person();
+  // Ali.name = 'Ali Khan';
+  // Ali.printPersonDetail();
+  Camera xz50Tmpro = Camera();
+  xz50Tmpro.brandName = 'Canon';
+  xz50Tmpro.color = 'Black';
+  xz50Tmpro.megapixel = '51';
+  xz50Tmpro.display = '3.2';
+  print(xz50Tmpro.printDetail());
 }
