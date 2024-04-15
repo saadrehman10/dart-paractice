72941d59098ab8af48c3fd4849d92d0a5ff9ffe8
Saad ur Rehman <125679304+saadrehman10@users.noreply.github.com>
2024-04-15 14:04:19 +0500

    update

---
+++ opp in dart/inherentance.dart
@@ -0,0 +1,25 @@
+// Super Class
+class Car {
+  String? name;
+  String? licenceNo;
+  int? year;
+  String? made;
+  Car(this.name, this.year, this.licenceNo, this.made);
+  void dispalyCar() {
+    print(
+        'The model of the car is $name model $year with licence $licenceNo and manifacrure of the car is $made');
+  }
+}
+
+class SuperCar extends Car {
+  SuperCar() : super('Lambo', 2020, "UAW-223", "Lamborgini");
+}
+
+class Gari extends SuperCar {
+  Gari();
+}
+
+void main() {
+  SuperCar lambo = SuperCar();
+  lambo.dispalyCar();
+}
