5caf97be02d8089c3379db469b72a9f855189366
Saad ur Rehman <125679304+saadrehman10@users.noreply.github.com>
2024-04-16 11:51:40 +0500

    update

--- opp in dart/encapsulation.dart
+++ opp in dart/encapsulation.dart
@@ -4,18 +4,43 @@ class Person {
   int? _age;
   String? _cast;
 
-  Person(this._name, this._age, this._cast);
+  String getName() {
+    return _name!;
+  }
+
+  int getAge() {
+    return _age!;
+  }
+
+  String getCast() {
+    return _cast!;
+  }
+
+  void setName(String name) {
+    this._name = name;
+  }
+
+  void setCast(String cast) {
+    this._cast = cast;
+  }
+  void setAge(int age) {
+    this._age = age;
+  }
+
   void display() {
     print('$_name $_age $_cast');
   }
 }
 
 void main() {
-  Person person1 = new Person("John Doe", 30, "Actor");
-  Person person2 = new Person("Jane Smith", 45, null);
+  // Person person1 = new Person("John Doe", 30, "Actor");
+  // Person person2 = new Person("Jane Smith", 45, null);
 
-  person1.display();
-  person2.display();
+  // person1.display();
+  // person2.display();
 
-  print(person1._age);
+  // print(person1._age);
+
+
+
 }
