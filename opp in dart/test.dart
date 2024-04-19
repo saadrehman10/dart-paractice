ad0a79341d8e9500dab75323a58381995390f8d7
Saad ur Rehman <125679304+saadrehman10@users.noreply.github.com>
2024-04-19 22:26:10 +0500

    update

--- opp in dart/test.dart
+++ opp in dart/test.dart
@@ -1,14 +1,14 @@
 import './encapsulation.dart';
 
 void main() {
-  Car mycar = new Car('Toyota', 2000);
-  mycar.printDetails();
+  // Car mycar = new Car('Toyota', 2000);
+  // mycar.printDetails();
 
-  Person person3 = Person();
-  person3.setAge(10);
-  print(person3.getAge());
-  person3.display();
-  // print(person3._age); not possible because of the sepreate file or libiary  encapsulation.dart but can acces the car
-  //so
-  print(mycar.name);
+  // Person person3 = Person();
+  // person3.setAge(10);
+  // print(person3.getAge());
+  // person3.display();
+  // // print(person3._age); not possible because of the sepreate file or libiary  encapsulation.dart but can acces the car
+  // //so
+  // print(mycar.name);
 }
