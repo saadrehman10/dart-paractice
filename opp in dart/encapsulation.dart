0d0afec263c4ad509110418acfc83d3291372c28
Saad ur Rehman <125679304+saadrehman10@users.noreply.github.com>
2024-04-19 22:19:07 +0500

    update

--- opp in dart/encapsulation.dart
+++ opp in dart/encapsulation.dart
@@ -226,6 +226,17 @@ void main() {
   newPlanet001.PlanetDetails();
   print('The sun to moon raito is ${newPlanet001.moonToSunRatio}');
 
+  Company company1 = new Company('Samsung',1919);
+
+company1.yearlyDebt = 1199.0;
+print(company1.yearlyDebt);
+print(company1._yearlyProfit);
+print(company1.mapedComanyData);
+company1.yearlyProfit = 2000000.0;
+print(company1.mapedComanyData);
+
+
+Company company2  = new Company('Apple',1950);
 
 
 
