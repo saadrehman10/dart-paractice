e51d8d18a1f477c415f9819be3747485c231b757
Saad ur Rehman <125679304+saadrehman10@users.noreply.github.com>
2024-04-20 17:56:21 +0500

    update

--- opp in dart/inherentance.dart
+++ opp in dart/inherentance.dart
@@ -4,7 +4,7 @@ class Car {
   String? licenceNo;
   int? year;
   String? made;
-  Car(this.name, this.year, this.licenceNo, this.made);
+  // Car(this.name, this.year, this.licenceNo, this.made);
   void dispalyCar() {
     print(
         'The model of the car is $name model $year with licence $licenceNo and manifacrure of the car is $made');
@@ -12,15 +12,24 @@ class Car {
 }
 
 class SuperCar extends Car {
-  SuperCar() : super('Lambo', 2020, "UAW-223", "Lamborgini");
+  // SuperCar() : super('Lambo', 2020, "UAW-223", "Lamborgini");
+  double? topSpeed;
+  int? yearlyTaxses;
+  double fine = 0.0;
+
+  void ticketsCalulations() {
+    fine = (topSpeed! / yearlyTaxses!) * 100;
+  }
+
+  void displaySuperCarDetails() {
+    this.dispalyCar();
+    print('with a top speed of $topSpeed  km/h');
+  }
 }
 
-class Gari extends SuperCar {
-  Gari();
-}
-
-
-
+// class Gari extends SuperCar {
+//   Gari();
+// }
 
 void main() {
   // SuperCar lambo = SuperCar();
@@ -29,5 +38,14 @@ void main() {
   // cultus.dispalyCar();
   // Gari civic = Gari();
   // civic.dispalyCar();
+  SuperCar car1 = new SuperCar();
+  car1.name = 'supra';
+  car1.year = 2018;
+  car1.made = 'Toyota';
+  car1.licenceNo = 'UAE-1000';
+  car1.topSpeed = 567;
+  car1.yearlyTaxses = 10000;
+  car1.displaySuperCarDetails();
+  
 
 }
