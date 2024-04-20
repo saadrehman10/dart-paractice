0bcacbbdcb26bb4484253bcc50797a8b82b067d7
Saad ur Rehman <125679304+saadrehman10@users.noreply.github.com>
2024-04-20 21:31:23 +0500

    update

--- opp in dart/inherentance.dart
+++ opp in dart/inherentance.dart
@@ -55,6 +55,18 @@ class Features extends Suv {
 //   Gari();
 // }
 
+class FeatuerClass2 extends Features {
+  String? random;
+  int? numramdom;
+  void diplayFE2() {
+    this.displaySuvDetails();
+    this.displayFeatures();
+    this.dispalyCar();
+    print('------');
+    print('$random , $numramdom');
+  }
+}
+
 void main() {
   // SuperCar lambo = SuperCar();
   // lambo.dispalyCar();
@@ -62,29 +74,44 @@ void main() {
   // cultus.dispalyCar();
   // Gari civic = Gari();
   // civic.dispalyCar();
-  SuperCar car1 = new SuperCar();
-  car1.name = 'supra';
-  car1.year = 2018;
-  car1.made = 'Toyota';
-  car1.licenceNo = 'UAE-1000';
-  car1.topSpeed = 567;
-  car1.yearlyTaxses = 10000;
-  car1.displaySuperCarDetails();
-  car1.dispalyCar();
-  car1.ticketsCalulations();
-  print(car1.fine);
+  // SuperCar car1 = new SuperCar();
+  // car1.name = 'supra';
+  // car1.year = 2018;
+  // car1.made = 'Toyota';
+  // car1.licenceNo = 'UAE-1000';
+  // car1.topSpeed = 567;
+  // car1.yearlyTaxses = 10000;
+  // car1.displaySuperCarDetails();
+  // car1.dispalyCar();
+  // car1.ticketsCalulations();
+  // print(car1.fine);
 
-  Suv car2 = new Suv();
-  car2.name = 'Hiroof';
-  car2.year = 1900;
-  car2.made = 'Suzuki';
-  car2.licenceNo = 'USA-999';
-  car2.seats = 8;
-  car2.fuleAvg = 22.3;
-  car2.isSportsEdition = false;
-  car2.displaySuvDetails();
+  // Suv car2 = new Suv();
+  // car2.name = 'Hiroof';
+  // car2.year = 1900;
+  // car2.made = 'Suzuki';
+  // car2.licenceNo = 'USA-999';
+  // car2.seats = 8;
+  // car2.fuleAvg = 22.3;
+  // car2.isSportsEdition = false;
+  // car2.displaySuvDetails();
 
-  Features car3 = new Features();
+  // Features car3 = new Features();
+
+  // car3.name = 'Hiroof';
+  // car3.year = 1900;
+  // car3.made = 'Suzuki';
+  // car3.licenceNo = 'USA-999';
+  // car3.seats = 8;
+  // car3.fuleAvg = 22.3;
+  // car3.isSportsEdition = false;
+  // car3.ac = false;
+  // car3.butterflyDoor = true;
+  // car3.fwd = true;
+  // car3.sunroof = false;
+  // car3.displayFeatures();
+
+  FeatuerClass2 car3 = new FeatuerClass2();
 
   car3.name = 'Hiroof';
   car3.year = 1900;
@@ -97,5 +124,7 @@ void main() {
   car3.butterflyDoor = true;
   car3.fwd = true;
   car3.sunroof = false;
-  car3.displayFeatures();
+  car3.random = 'third inhreantance class property';
+  car3.numramdom = 4834823084238498;
+  car3.diplayFE2();
 }
