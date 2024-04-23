774e8b7b2b40afe43571d9fc9803f6a1acfe1d19
Saad ur Rehman <125679304+saadrehman10@users.noreply.github.com>
2024-04-23 15:11:56 +0500

    update

--- opp in dart/static.dart
+++ opp in dart/static.dart
@@ -1,7 +1,32 @@
+class Person {
+  String? firstName;
+  String? lastName;
+  int? dob;
+  int? _cnicNO;
+
+  Person(this.firstName, this.lastName, this.dob);
+  Person.allAttributes(this.firstName, this.lastName, this.dob, this._cnicNO);
+  Person.cons2({this.firstName, this.lastName, this.dob});
+
+  set cincNO(int number) => number.toString().length == 14
+      ? _cnicNO = number
+      : print('The cnic Length is invalid');
+  int get cnicNO => _cnicNO!;
+  String get fullName => '$firstName $lastName';
+
+  int calulateAge() {
+    return this.dob != null ? 2024 - this.dob! : -1;
+  }
+
+  void showPersonalInfo() {
+    print("Name: ${this.fullName}");
+    print("Age : ${this.calulateAge()}");
+    print('${_cnicNO != null ? 'CNIC NO: ${this.cnicNO}' : ''}');
+  }
+}
 
 
+void main() {
 
-
-void main(){
 
 }
