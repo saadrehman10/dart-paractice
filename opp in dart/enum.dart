ad29a88377382d585619fd6ecdf7d590f0a3c322
Saad ur Rehman <125679304+saadrehman10@users.noreply.github.com>
2024-04-24 13:59:37 +0500

    enum

--- opp in dart/enum.dart
+++ opp in dart/enum.dart
@@ -1,4 +1,20 @@
-void main() {
+enum days { Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday }
 
-  
+class Week {
+  int? date;
+  days? day;
+  int? year;
+
+  Week(this.date, this.day, this.year);
+  void printDate() {
+    print('$date / $day / $year');
+  }
+}
+
+void main() {
+  days day = days.Friday;
+  print(day);
+   Week randomDay = new Week(11,
+
+
 }
