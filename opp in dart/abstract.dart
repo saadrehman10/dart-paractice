bdde903df788b85a160eb2310216f5d2368b740e
Saad ur Rehman <125679304+saadrehman10@users.noreply.github.com>
2024-04-26 17:04:27 +0500

    update

--- opp in dart/abstract.dart
+++ opp in dart/abstract.dart
@@ -1,9 +1,32 @@
 abstract class DatabaseTypes {
   bool? dbisSQl;
   bool? dbisNonSQl;
+  String? discription;
+  String? nameOfdb;
+  int? _code;
+  static int checkdbRuns = 0;
 
+  DatabaseTypes(this.discription, this.nameOfdb, bool? type) {
+    type == true
+        ? (this.dbisSQl = true, this.dbisNonSQl = false)
+        : (this.dbisSQl = false, this.dbisNonSQl = true);
+  }
+
+  set code(int value) =>
+      value > 10 ? _code = value : 'Error the value must be smaller than 10';
+  int get code => _code!;
+
+  void checkdatabase() {
+    checkdbRuns++;
+    dbisSQl!
+        ? print('Your data base is SQL')
+        : print('Your data base is not SQL');
+  }
+
+  static void dbCheckinintotal() {
+    print(checkdbRuns);
+  }
+  
 }
 
-void main() {
- 
-}
+void main() {}
