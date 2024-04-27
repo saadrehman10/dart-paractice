9474aa042301982e2d1d58770e7005c36afd77f4
Saad ur Rehman <125679304+saadrehman10@users.noreply.github.com>
2024-04-27 18:36:55 +0500

    interface

--- opp in dart/interface-abs-inheitance.dart
+++ opp in dart/interface-abs-inheitance.dart
@@ -2,48 +2,20 @@ class Shapes {
   String? shapeName;
   String? areaformula;
   double area = 0;
+  bool? threeD;
   static int noOfSapes = 0;
 
   Shapes(this.shapeName, this.areaformula) {
     noOfSapes++;
   }
-  void calculateArea() {
-    List<String> formula = areaformula!.split('');
-    List<String> char = '*/+-=(){}[]'.split('');
-    List<String> nums = '01234567890'.split('');
-    double area = 0.0;
-    bool isNum (String no) => nums.contains(no) ;
-
-    for (int i = 0; i <= formula.length; i++) {
-      if (char.contains(formula[i])) {
-        switch (formula[i]) {
-          case '*':
-            if (isNum(formula[i-1]) && isNum(formula[i+1]) ){
-              area += double.parse(int.parse(formula[i-1]) * int.parse(formula[i+1]));
-            } else {
-              area *= double.parse(formula[i - 1]);
-            }
-          case '/':
-            area /= area;
-          case '+':
-            area *= area;
-          case '-':
-            area *= area;
-          case '[' || ']':
-            area *= area;
-          case '{' || '}':
-            area *= area;
-          case '(' || ')':
-            area *= area;
-          default:
-            continue;
-        }
-      }
-    }
+  void formulaArea() {
+    
+        
   }
+  
 
   void displayShapeDetails() {
-    print('$shapeName ');
+    
   }
 }
 
