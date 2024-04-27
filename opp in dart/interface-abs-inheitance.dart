2348af12f27512ecaceed0b24d805a184c7a54ea
Saad ur Rehman <125679304+saadrehman10@users.noreply.github.com>
2024-04-27 10:35:28 +0500

    interface

--- opp in dart/interface-abs-inheitance.dart
+++ opp in dart/interface-abs-inheitance.dart
@@ -9,7 +9,28 @@ class Shapes {
   }
   void calculateArea() {
     List<String> fromula = areaFromula!.split('');
-    
+    List<String> char = '1234567890*/+-=(){}[]'.split('');
+    for(int i = 0 ; i <= fromula.length; i++ ){
+      if (char.contains(fromula[i])){
+        continue;
+      }else{
+        var num = double.parse(fromula[i]);
+        if (i == 0){
+          area += num;
+        }else{
+          switch (fromula[i -  1]){
+            case '*':
+              area *= num; break;
+            case '/':
+              area /= num; break;
+            case '+':
+              area += num; break;
+            case '-':
+              area -= num; break;
+            default:
+              print("Invalid formula");
+          }
+    }
   }
 }
 
