d7d82c5d50100baf82686080352dd28679f27155
Saad ur Rehman <125679304+saadrehman10@users.noreply.github.com>
2024-04-27 10:52:11 +0500

    interface

--- opp in dart/interface-abs-inheitance.dart
+++ opp in dart/interface-abs-inheitance.dart
@@ -1,47 +1,28 @@
 class Shapes {
   String? shapeName;
-  String? areaFromula;
+  String? areaformula;
   double area = 0;
   static int noOfSapes = 0;
 
-  Shapes(this.shapeName, this.areaFromula) {
+  Shapes(this.shapeName, this.areaformula) {
     noOfSapes++;
   }
   void calculateArea() {
-    List<String> fromula = areaFromula!.split('');
-    List<String> char = '1234567890*/+-=(){}[]'.split('');
-    for (int i = 0; i <= fromula.length; i++) {
-      if (char.contains(fromula[i])) {
-        continue;
-      } else {
-        var num = double.parse(fromula[i]);
-        if (i == 0) {
-          area += num;
-        } else {
-          switch (fromula[i - 1]) {
-            case '*':
-              area *= num;
-              break;
-            case '/':
-              area /= num;
-              break;
-            case '+':
-              area += num;
-              break;
-            case '-':
-              area -= num;
-              break;
-            default:
-              print("Invalid formula");
-          }
+    List<String> formula = areaformula!.split('');
+    List<String> char =  '*/+-=(){}[]'.split('');
+    for (int i = 0; i <= formula.length; i++) {
+      if (char.contains(formula[i])) {
+        switch(formula[i]){
+          case '*':
+
         }
       }
     }
   }
+
+  void displayShapeDetails() {
+    print('$shapeName ');
+  }
 }
 
-void main() {
-
-
-  
-}
+void main() {}
