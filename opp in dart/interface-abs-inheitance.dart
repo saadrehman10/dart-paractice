22cf7f3b9ed0dcc631c076c3cbf157465627f614
Saad ur Rehman <125679304+saadrehman10@users.noreply.github.com>
2024-04-27 18:56:37 +0500

    update

--- opp in dart/interface-abs-inheitance.dart
+++ opp in dart/interface-abs-inheitance.dart
@@ -5,17 +5,32 @@ class Shapes {
   bool? threeD;
   static int noOfSapes = 0;
 
-  Shapes(this.shapeName, this.areaformula) {
+  Shapes(this.shapeName, this.areaformula, this.threeD) {
     noOfSapes++;
   }
   void formulaArea() {
-    
-        
+    print(areaformula);
   }
-  
 
   void displayShapeDetails() {
-    
+    print('Shape name : $shapeName');
+  }
+}
+
+class ThreeDshape implements Shapes {
+  String? shapeName;
+  String? areaformula;
+  double area = 0;
+  bool? threeD;
+  ThreeDshape(String? name, String? areaFormula);
+
+  @override
+  void formulaArea() {
+    print(areaformula);
+  }
+  @override
+  void displayShapeDetails() {
+    print('Shape name : $shapeName');
   }
 }
 
