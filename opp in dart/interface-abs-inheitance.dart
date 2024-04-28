3fc7716e0b9218d081779e7b0464ed0d8be814a3
Saad ur Rehman <125679304+saadrehman10@users.noreply.github.com>
2024-04-28 11:33:17 +0500

    update

--- opp in dart/interface-abs-inheitance.dart
+++ opp in dart/interface-abs-inheitance.dart
@@ -82,48 +82,52 @@ class Reactangel implements Shapes, TwoD {
 
 void main() {
   // Create instances of Shapes
-  var circle = Shapes('Circle', 'π * r^2', false);
-  var square = Shapes('Square', 'side * side', false);
+  // var circle = Shapes('Circle', 'π * r^2', false);
+  // var square = Shapes('Square', 'side * side', false);
 
-  // Create instances of ThreeDshape
-  var sphere = ThreeDshape('Sphere', '4/3 * π * r^3');
-  var cube = ThreeDshape('Cube', 'side^3');
+  // // Create instances of ThreeDshape
+  // var sphere = ThreeDshape('Sphere', '4/3 * π * r^3');
+  // var cube = ThreeDshape('Cube', 'side^3');
 
-  // Create instances of TwoD
-  var triangle = TwoD('Triangle', '0.5 * base * height');
-  var rectangle = TwoD('Rectangle', 'length * width');
+  // // Create instances of TwoD
+  // var triangle = TwoD('Triangle', '0.5 * base * height');
+  // var rectangle = TwoD('Rectangle', 'length * width');
 
-  // Create instances of Reactangel
-  var reactangel1 = Reactangel('Reactangel', 'length * width');
-  var reactangel2 = Reactangel('Reactangel', 'length * width');
+  // // Create instances of Reactangel
+  // var reactangel1 = Reactangel('Reactangel', 'length * width');
+  // var reactangel2 = Reactangel('Reactangel', 'length * width');
 
-  // List to store all instances
-  var shapesList = [circle, square, sphere, cube, triangle, rectangle, reactangel1, reactangel2];
+  // // List to store all instances
+  // var shapesList = [circle, square, sphere, cube, triangle, rectangle, reactangel1, reactangel2];
 
-  // Additional instances to meet the requirement of 10-11 objects
-  var hexagon = Shapes('Hexagon', '(3 * sqrt(3) / 2) * side^2', false);
-  var cylinder = ThreeDshape('Cylinder', '2 * π * r * (r + h)');
-  var ellipse = TwoD('Ellipse', 'π * a * b');
+  // // Additional instances to meet the requirement of 10-11 objects
+  // var hexagon = Shapes('Hexagon', '(3 * sqrt(3) / 2) * side^2', false);
+  // var cylinder = ThreeDshape('Cylinder', '2 * π * r * (r + h)');
+  // var ellipse = TwoD('Ellipse', 'π * a * b');
 
-  // Adding new instances to the list
-  shapesList.addAll([hexagon, cylinder, ellipse]);
+  // // Adding new instances to the list
+  // shapesList.addAll([hexagon, cylinder, ellipse]);
 
-  // Display details for each shape
-  // for (var shape in shapesList) {
+  // // Display details for each shape
+  // // for (var shape in shapesList) {
     
-  //   print('Number of Shapes: ${Shapes.noOfSapes}');
-  //   print('---------------------------');
-  // }
+  // //   print('Number of Shapes: ${Shapes.noOfSapes}');
+  // //   print('---------------------------');
+  // // }
 
-  // Specific method for Reactangel
-  reactangel1.thirdfun();
-  reactangel2.thirdfun();
+  // // Specific method for Reactangel
+  // reactangel1.thirdfun();
+  // reactangel2.thirdfun();
 
-  // Specific method for ThreeDshape
-  sphere.thirdfun();
-  cube.thirdfun();
+  // // Specific method for ThreeDshape
+  // sphere.thirdfun();
+  // cube.thirdfun();
 
-  // Specific method for TwoD
-  triangle.thirdfun();
-  rectangle.thirdfun();
+  // // Specific method for TwoD
+  // triangle.thirdfun();
+  // rectangle.thirdfun();
+  
+ 
+
+
 }
