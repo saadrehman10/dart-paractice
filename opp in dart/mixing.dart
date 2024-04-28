9d38f5898d3a8eb03158771ce32dadb2408b98c5
Saad ur Rehman <125679304+saadrehman10@users.noreply.github.com>
2024-04-28 12:28:09 +0500

    mixing

--- opp in dart/mixing.dart
+++ opp in dart/mixing.dart
@@ -1,14 +1,17 @@
 mixin mixing1 {
   static int? a;
-  void dispay() {
+  int? b;
+  void dispay1() {
     print('this is mixing one');
     print(a);
   }
+
+  void setValue() {}
 }
 
 mixin mixing2 {
   int? a;
-  void dispay() {
+  void dispay2() {
     print('this is mixing two');
     print(a);
   }
@@ -20,9 +23,21 @@ class Random with mixing1, mixing2 {
   }
 }
 
+class Ranodm2 extends Random {
+  void random2funciton() {
+    print('random2');
+  }
+}
+
 void main() {
   Random random = Random();
   random.a = 500;
-  random.dispay();
+  random.dispay1();
+  random.dispay2();
   random.randmomfunction();
+  Ranodm2 random2 = Ranodm2();
+  random2.b = 100;
+  random.dispay1();
+  random2.dispay2();
+  random2.random2funciton();
 }
