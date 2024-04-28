8f9516629a5c30dbef3c0221b5cd0f7871a39686
Saad ur Rehman <125679304+saadrehman10@users.noreply.github.com>
2024-04-28 12:40:23 +0500

    mixing

--- opp in dart/mixing.dart
+++ opp in dart/mixing.dart
@@ -1,3 +1,4 @@
+
 mixin mixing1 {
   static int? a;
   int? b;
@@ -29,15 +30,40 @@ class Ranodm2 extends Random {
   }
 }
 
+class Random3 {
+  void random3funciton() {
+    print('random3');
+  }
+
+  void random3function2() {
+    print('this is second fun of random3');
+  }
+}
+
+mixin mx on Random3 {
+  @override
+  void random3funciton() {
+    print('This is mixing mx overrideng fun of random4');
+  }
+
+  void mixingfunran4() {
+    print('This is mixining fun of random4');
+  }
+}
+
 void main() {
-  Random random = Random();
-  random.a = 500;
-  random.dispay1();
-  random.dispay2();
-  random.randmomfunction();
-  Ranodm2 random2 = Ranodm2();
-  random2.b = 100;
-  random.dispay1();
-  random2.dispay2();
-  random2.random2funciton();
+  // Random random = Random();
+  // random.a = 500;
+  // random.dispay1();
+  // random.dispay2();
+  // random.randmomfunction();
+  // Ranodm2 random2 = Ranodm2();
+  // random2.b = 100;
+  // random.dispay1();
+  // random2.dispay2();
+  // random2.random2funciton();
+  Random3 ran = Random3();
+  ran.random3funciton();
+  ran.random3function2();
+ 
 }
