d637cba7a70e4240d30dc1a8d42f5a99781e5684
Saad ur Rehman <125679304+saadrehman10@users.noreply.github.com>
2024-04-29 20:53:48 +0500

    update

--- opp in dart/factoryCons.dart
+++ opp in dart/factoryCons.dart
@@ -4,13 +4,33 @@ class Shopes {
   String? address;
   int? _sellerCode;
 
+  Shopes._internal(this.name, this.product, this.address);
+
   factory Shopes(String name, String product, String area) {
-    name = name;
-    product = product;
-    
+    if (area == "south") {
+      throw Exception('We dont operate there ');
+    } else if (area == 'north') {
+      print('We operate there');
+      return Shopes._internal(name, product, area);
+    } else if (area == 'west') {
+      throw Exception('We dont operate there ');
+    } else if (area == 'east') {
+      throw Exception('We dont operate there ');
+    }
+    return Shopes._internal(name, product, null);
+  }
+
+  int get sellerCode => _sellerCode!;
+  set sellerCode(int value) => _sellerCode = value;
+
+  void displayShopInfo() {
+    print("Name: $name");
+    print("Product: $product");
+    print("Address: $address");
+    print('${_sellerCode != null? '${_sellerCode}':'\n'}');
   }
 }
 
 void main() {
-  Shopes shop1 = new Shopes('shpeA', 'shoes', '234');
+  Shopes shop1 = new Shopes('shpeA', 'shoes', 'north');
 }
