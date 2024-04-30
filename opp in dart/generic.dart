152cd6374172fbda912a054750a35fb9fe91956b
Saad ur Rehmah <125679304+saadrehman10@users.noreply.github.com>
2024-04-30 22:32:29 +0500

    update

--- opp in dart/generic.dart
+++ opp in dart/generic.dart
@@ -3,7 +3,7 @@ class Someone {
   String? dob;
   int? age;
 
-  Someone._internal(this.name, this.dob, this.age);
+  Someone._internal(this.name, this.dob, [this.age]);
   factory Someone(String? name, String? dob) {
     List<String> year = dob!.split('/');
     int? newDob;
@@ -12,6 +12,30 @@ class Someone {
     }
     newDob = 2024 - newDob!;
     return Someone._internal(name, dob, newDob);
+  }
+  void displaySomeone() {
+    print('$name is $age years old');
+  }
+}
+
+mixin mx1 on Someone {
+  String? gender;
+  @override
+  void displaySomeone() {
+    print('$name is $age years old ${gender! == 'Mail' ? 'he' : 'She'}');
+  }
+}
+
+class AnotherOne extends Someone with mx1 {
+  String? gender;
+  AnotherOne._internal(String? name, String? dob, String? gender)
+      : super._internal(name, dob);
+  factory AnotherOne(String? name, String? dob, String? gender) {
+    if (gender == 'Mail' || gender == 'Female')
+      throw Exception('no gender exist');
+    else {
+      return AnotherOne._internal(name, dob, gender);
+    }
   }
 }
 
