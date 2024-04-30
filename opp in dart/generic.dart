7e39d70e1c310633193fabbe2ad46430cac74819
Saad ur Rehman <saadrehman10>
2024-04-30 17:37:54 +0500

    update

--- opp in dart/generic.dart
+++ opp in dart/generic.dart
@@ -1,7 +1,8 @@
-class Someone {
+class Someone{
  String? name;
  String? dob;
  int? age;
+ 
  
  Someone._internal(this.name, this.dob, this.age) ;
  factory Someone(String? name , String? dob){
@@ -10,8 +11,8 @@ class Someone {
    for(int i = 0; i < year.length ; i++){
      year[i].length == 4 ? newDob = int.parse(year[i]) : null;
    }
-   print(newDob);
-   return Someone._internal(name, dob );
+   newDob = 2024 - newDob;
+   return Someone._internal(name, dob, newDob);
  }
 }
 
