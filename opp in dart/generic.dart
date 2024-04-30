3c3b84a742412b7dadbbd13ac118bae63bbd7038
Saad ur Rehman <saadrehman10>
2024-04-30 17:41:49 +0500

    update

--- opp in dart/generic.dart
+++ opp in dart/generic.dart
@@ -15,6 +15,15 @@ class Someone{
    return Someone._internal(name, dob, newDob);
  }
 }
+class IntData {
+  int data;
+  IntData(this.data);
+}
+// Creating a class for double
+class DoubleData {
+  double data;
+  DoubleData(this.data);
+}
 
 void main(){
   Someone s = new Someone('John','1990/01/01');
