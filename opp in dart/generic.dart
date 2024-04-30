165f2a20f61ece2583f283df19a784c925687ad1
Saad ur Rehman <saadrehman10>
2024-04-30 17:28:24 +0500

    update

--- opp in dart/generic.dart
+++ opp in dart/generic.dart
@@ -3,18 +3,19 @@ class Someone {
  String? dob;
  int? age;
  
- Someone._internal(this.name, this.dob) , this.age;
- factory Someone({String? name , String? dob}){
+ Someone._internal(this.name, this.dob, this.age) ;
+ factory Someone(String? name , String? dob){
    List<String> year = dob!.split('/');
    int? newDob;
-   for(int i = 1; i =< year.length ; i++){
+   for(int i = 0; i < year.length ; i++){
      year[i].length == 4 ? newDob = int.parse(year[i]) : null;
    }
-   return new Someone._internal(name, dob , age);
+   print(newDob);
+   return Someone._internal(name, dob );
  }
 }
 
 void main(){
-  Someone s = new Someone(name: 'John', dob: '1990/01/01');
+  Someone s = new Someone('John','1990/01/01');
  
 }
