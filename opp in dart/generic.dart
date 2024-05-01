2c958c24d02fddcacc39106110d34b607b4a2771
Saad ur Rehmah <125679304+saadrehman10@users.noreply.github.com>
2024-05-01 12:30:18 +0500

    update

--- opp in dart/generic.dart
+++ opp in dart/generic.dart
@@ -22,7 +22,7 @@ mixin mx1 on Someone {
   String? gender;
   @override
   void displaySomeone() {
-    print('$name is $age years old ${gender! == 'Mail' ? 'he' : 'She'}');
+    print('$name is $age years old ${gender! == 'Male' ? 'he' : 'She'}');
   }
 }
 
@@ -31,7 +31,7 @@ class AnotherOne extends Someone with mx1 {
   AnotherOne._internal(String? name, String? dob, String? gender)
       : super._internal(name, dob);
   factory AnotherOne(String? name, String? dob, String? gender) {
-    if (gender == 'Mail' || gender == 'Female')
+    if (gender != 'Male' || gender != 'Female')
       throw Exception('no gender exist');
     else {
       return AnotherOne._internal(name, dob, gender);
@@ -42,4 +42,6 @@ class AnotherOne extends Someone with mx1 {
 void main() {
   Someone s = new Someone('John', '1990/01/01');
   print(s.age);
+  AnotherOne perosn = new AnotherOne('smith', '01/01/1992','Mail');
+  perosn.displaySomeone();
 }
