f7a59b9afa05ed18288ed91fa336e290637fe727
Saad ur Rehmah <125679304+saadrehman10@users.noreply.github.com>
2024-05-01 15:44:14 +0500

    update

--- opp in dart/factoryCons.dart
+++ opp in dart/factoryCons.dart
@@ -201,4 +201,12 @@ void main() {
   // // print first and last name
   // print("From normal constructor: ${person.firstName} ${person.lastName}");
   // print("From factory constructor: ${person2.firstName} ${person2.lastName}");
+    Someone s = new Someone('John', '1990/01/01');
+  print(s.age);
+  AnotherOne person2 = new AnotherOne('smantha', '01/01/2000', 'Female');
+  AnotherOne person = new AnotherOne('smith', '01/01/1992', 'Male');
+
+  person.displaySomeone();
+  print('---------------');
+  person2.displaySomeone();
 }
