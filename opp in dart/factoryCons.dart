c0a874cfd21856d56b8f5470d05a504996c34311
Saad ur Rehmah <125679304+saadrehman10@users.noreply.github.com>
2024-05-01 15:43:06 +0500

    update

--- opp in dart/factoryCons.dart
+++ opp in dart/factoryCons.dart
@@ -89,40 +89,7 @@ class Singleton {
 //   }
 // }
 // enum ShapeType
-enum ShapeType { circle, rectangle }
 
-// abstract class Shape
-abstract class Shape {
-  // factory constructor
-  factory Shape(ShapeType type) {
-    switch (type) {
-      case ShapeType.circle:
-        return Circle();
-      case ShapeType.rectangle:
-        return Rectangle();
-      default:
-        throw 'Invalid shape type';
-    }
-  }
-  // method
-  void draw();
-}
-
-class Circle implements Shape {
-  // implement draw method
-  @override
-  void draw() {
-    print('Drawing circle');
-  }
-}
-
-class Rectangle implements Shape {
-  // implement draw method
-  @override
-  void draw() {
-    print('Drawing rectangle');
-  }
-}
 class Person {
   String firstName;
   String lastName;
