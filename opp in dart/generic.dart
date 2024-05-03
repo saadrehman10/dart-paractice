ba0f3e62fced897cdbea8c6410f42aa727850a24
Saad ur Rehmah <125679304+saadrehman10@users.noreply.github.com>
2024-05-03 21:15:39 +0500

    update

--- opp in dart/generic.dart
+++ opp in dart/generic.dart
@@ -6,12 +6,19 @@ class Patient {
   Patient(this.name, this.diagnostic);
   Patient._named(this.name, this.diagnostic);
   factory Patient.named(String? name, String? diagnostic) {
-    if(){
+    if (name != null && diagnostic != null) {
       return Patient._named(name, diagnostic);
-    }else{
+    } else {
       throw Exception('Wrong input detalis');
     }
   }
+
+  set ptCode(int? code) => code!.toString().length == 10
+      ? _ptCode = code
+      : print('worng lenght should be 10');
+
+  int get ptCode => _ptCode!;
+
 }
 
 void main() {}
