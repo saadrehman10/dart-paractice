8ad271fc0ba7657bac415f928f624f7594dd2365
Saad ur Rehmah <125679304+saadrehman10@users.noreply.github.com>
2024-05-03 20:53:38 +0500

    update

--- opp in dart/generic.dart
+++ opp in dart/generic.dart
@@ -1,2 +1,13 @@
+class Patient {
+  String? name;
+  String? diagnostic;
+  int? _ptCode;
+
+  Patient(this.name, this.diagnostic);
+  Patient._named(this.name, this.diagnostic);
+  factory Patient(String? name, String? diagnostic) {
+    if(){}
+  }
+}
 
 void main() {}
