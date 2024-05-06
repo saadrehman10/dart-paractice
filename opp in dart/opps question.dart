1f74b2bfea86f7736f381cefaf9fa93f959ca3e2
Saad ur Rehmah <125679304+saadrehman10@users.noreply.github.com>
2024-05-06 10:49:46 +0500

    changes

--- opp in dart/opps question.dart
+++ opp in dart/opps question.dart
@@ -1,1 +1,16 @@
+//Q1 writer a dart programe to create class laptop with properties [id,name,ram] and create 3 objects of of it and print all details
+
+class Laptop {
+  int? id;
+  String? name;
+  double? ram;
+
+  Laptop(this.id, this.name, this.ram);
+  void dispaly() {
+    print('Laptop id : $this.id');
+    print('Laptop name : $this.name');
+    print('Laptop ram : $this.ram');
+  }
+}
+
 void main() {}
