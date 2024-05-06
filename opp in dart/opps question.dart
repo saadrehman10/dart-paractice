2e0e94b523d564e54db119c70ec6f9070d9ad36c
Saad ur Rehmah <125679304+saadrehman10@users.noreply.github.com>
2024-05-06 10:53:23 +0500

    changes

--- opp in dart/opps question.dart
+++ opp in dart/opps question.dart
@@ -7,10 +7,17 @@ class Laptop {
 
   Laptop(this.id, this.name, this.ram);
   void dispaly() {
-    print('Laptop id : $this.id');
-    print('Laptop name : $this.name');
-    print('Laptop ram : $this.ram');
+    print('Laptop id : ${this.id}');
+    print('Laptop name : ${this.name}');
+    print('Laptop ram : ${this.ram }GB');
   }
 }
 
-void main() {}
+void main() {
+  Laptop laptop1 = Laptop(1123, 'samsung cromebook', 8.00);
+  Laptop laptop2 = Laptop(2233, 'Apple mackbook', 4.00);
+  Laptop laptop3 = Laptop(1222, 'Thinkpad', 16.00);
+  laptop3.dispaly();
+  laptop2.dispaly();
+  laptop1.dispaly();
+}
