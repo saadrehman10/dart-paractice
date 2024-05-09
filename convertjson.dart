48bce11d3d58e65490a2d62bd25c057a4247bed9
Saad ur Rehmah <125679304+saadrehman10@users.noreply.github.com>
2024-05-09 09:49:36 +0500

    changes

--- convertjson.dart
+++ convertjson.dart
@@ -4,17 +4,15 @@ class nullValeus {
   String? hello;
   List<int?>? newword;
 
-  void printAddress(String address) {
-  print(address);
-}
-
+  void printAddress(String? address) {
+    print(address);
+  }
 }
 
 void main() {
   nullValeus obj1 = new nullValeus();
-  obj1.newword = obj1.newword ?? [1,234,null,null, 32];
-   List<int?> items = [1, 2, null, 4];
+  obj1.newword = obj1.newword ?? [1, 234, null, null, 32];
+  List<int?> items = [1, 2, null, 4];
   print(items);
-   
-
+  obj1.printAddress(null);
 }
