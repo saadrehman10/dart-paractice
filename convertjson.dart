d34c5a36800970d5e0f4dce507490f848812c7aa
Saad ur Rehmah <125679304+saadrehman10@users.noreply.github.com>
2024-05-11 19:09:15 +0500

    changes

--- convertjson.dart
+++ convertjson.dart
@@ -77,12 +77,20 @@ void main() {
   // late var name;
   // name = 'hello world';
   // print(name);
-
+  late final String name;
   late var temp = lateword.fun1();
   print(lateword.count);
   var temp2 = lateword.fun1();
   print(lateword.count);
   print(temp);
+  try {
+    print(name);
+  } catch (e) {
+    print('the name is not yet declare');
+    
+  }
+
+  name = 'hello';
   print(lateword.count);
-  
+  print(name);
 }
