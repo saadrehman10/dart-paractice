f8699c743f3a8070ef71f3a949a14aa6c8fbcadb
Saad ur Rehmah <125679304+saadrehman10@users.noreply.github.com>
2024-05-11 22:31:22 +0500

    changes

---
+++ nullsafty.dart
@@ -0,0 +1,13 @@
+// ? is to declar that declared var could be null
+
+void main() {
+  late String? address;
+  address = 'US';
+  print(address);
+  // How to declar a nullable type var in dart
+  String? name;
+  var name2;
+   dynamic name3 = null;
+   
+
+}
