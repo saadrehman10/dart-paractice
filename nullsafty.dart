128dc865e3223e33af49677737630962789a7622
Saad ur Rehmah <125679304+saadrehman10@users.noreply.github.com>
2024-05-12 21:40:02 +0500

    changes

--- nullsafty.dart
+++ nullsafty.dart
@@ -21,14 +21,18 @@ class Person{
 
 
 void main() {
-  late String? address;
-  address = 'US';
-  print(address);
-  // How to declar a nullable type var in dart
-  String? name;
-  var name2;
-  dynamic name3 = null;
-  int? age;
-  print(nullchecker(null));
-  print(nullchecker(33));
+//   late String? address;
+//   address = 'US';
+//   print(address);
+//   // How to declar a nullable type var in dart
+//   String? name;
+//   var name2;
+//   dynamic name3 = null;
+//   int? age;
+//   print(nullchecker(null));
+//   print(nullchecker(33));
+
+   Person person = Person();
+    person.setName("Mark");
+    print(person.name);
 }
