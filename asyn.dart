325f76e71773f38f17ee5e645d9638880b9eccfa
Saad ur Rehmah <125679304+saadrehman10@users.noreply.github.com>
2024-05-12 22:21:35 +0500

    update

--- asyn.dart
+++ asyn.dart
@@ -1,4 +1,9 @@
 import 'dart:isolate';
 
-void main() async {}
-
+void main() async {
+  Future.delayed(Duration(seconds: 3), () => print('hello world'));
+  while (true) {
+    print('future befoe');
+  }
+  
+}
