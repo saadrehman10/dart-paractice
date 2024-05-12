fc3e78ab34f9be75cc3b02f5585fde02180bd797
Saad ur Rehmah <125679304+saadrehman10@users.noreply.github.com>
2024-05-12 23:13:46 +0500

    changes

--- asyn.dart
+++ asyn.dart
@@ -1,14 +1,13 @@
 import 'dart:isolate';
 
-void main()  {
+Future<List<String>> getname() {
+  return Future.value(['']);
+}
+
+void main() {
   Future.delayed(Duration(seconds: 3), () => print('hello world'));
   Future.delayed(Duration(seconds: 4), () => print('hello world'));
 
-   print('future befoe');
+  print('future befoe');
   Future.delayed(Duration(seconds: 3), () => print('hello world'));
-
-  // while (true) {
-   
-  // }
-  
 }
