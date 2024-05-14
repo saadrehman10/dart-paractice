d3db8ff26f4a9e24408a2016a8faf603b53f2502
Saad ur Rehmah <125679304+saadrehman10@users.noreply.github.com>
2024-05-14 11:02:28 +0500

    update

--- asyn.dart
+++ asyn.dart
@@ -3,19 +3,28 @@ import 'dart:isolate';
 Future<List<String>> getname() {
   return Future.delayed(Duration(seconds: 2), () => ['saad']);
 }
+void getData() async{
+  String data = await middleFunction();
+  print(data);
+}
 
+Future<String> middleFunction(){
+  return Future.delayed(Duration(seconds:5), ()=> "Hello");
+}
 void main() async {
   // Future.delayed(Duration(seconds: 3), () => print('hello world'));
   // Future.delayed(Duration(seconds: 4), () => print('hello world'));
 
   // print('future befoe');
   // Future.delayed(Duration(seconds: 3), () => print('hello world'));
+   print("Start");
+  getData();
+  print("End");
+//   print(getname());
+//   bool temp = true;
 
-  print(getname());
-  bool temp = true;
-
-  getname().then((value) {
-      print(value[0]);
-      temp = false;
-    });
-}
+//   getname().then((value) {
+//       print(value[0]);
+//       temp = false;
+//     });
+// }
