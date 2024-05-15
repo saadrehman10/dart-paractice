9dff02c21b03358405a35fad2705ca0d951d9366
Saad ur Rehmah <125679304+saadrehman10@users.noreply.github.com>
2024-05-15 14:27:37 +0500

    changes

--- stream.dart
+++ stream.dart
@@ -1,12 +1,16 @@
 class Steamsong {
   Stream<String> getUserName() async* {
-    await Future.delayed(Duration(seconds: 1));
+    await Future.delayed(Duration(seconds: 2));
     yield 'hello';
+    await Future.delayed(Duration(seconds: 1));
+    yield 'John';
+    await Future.delayed(Duration(seconds: 1));
+    yield 'Smith';
+    
   }
 }
 
 void main() {
-   
-
-
+  Steamsong obj = Steamsong();
+  print(obj.getUserName());
 }
