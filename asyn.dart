74cd55c7f0f7b13dbe41003fcc35dcfacfa5900c
Saad ur Rehmah <125679304+saadrehman10@users.noreply.github.com>
2024-05-15 09:01:24 +0500

    changes

--- asyn.dart
+++ asyn.dart
@@ -3,23 +3,32 @@ import 'dart:isolate';
 Future<List<String>> getname() {
   return Future.delayed(Duration(seconds: 2), () => ['saad']);
 }
+
 void getData() {
+  var data = middleFunction().then((value) => value);
+  print(data);
+}
+
+void getData2() {
   var data = middleFunction();
   print(data);
 }
 
-Future<String> middleFunction(){
-  return Future.delayed(Duration(seconds:5), ()=> "Hello");
+void getData3() async {
+  var data = await middleFunction();
+  print(data);
 }
-void main()  {
+
+Future<String> middleFunction() {
+  return Future.delayed(Duration(seconds: 3), () => "Hello");
+}
+
+void main() {
   // Future.delayed(Duration(seconds: 3), () => print('hello world'));
   // Future.delayed(Duration(seconds: 4), () => print('hello world'));
 
   // print('future befoe');
   // Future.delayed(Duration(seconds: 3), () => print('hello world'));
-   print("Start");
-  getData();
-  print("End");
 //   print(getname());
 //   bool temp = true;
 
@@ -27,4 +36,19 @@ void main()  {
 //       print(value[0]);
 //       temp = false;
 //     });
- }
+  print('----');
+  print("Start");
+  getData();
+  print("End");
+  print('----');
+  print('----');
+  print("Start");
+  getData2();
+  print("End");
+  print('----');
+  print('----');
+  print("Start");
+  getData3();
+  print("End");
+  print('----');
+}
