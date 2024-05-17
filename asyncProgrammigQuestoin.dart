99d8b5350fc765e4b1996b6699b26f900bcd66f2
Saad ur Rehmah <125679304+saadrehman10@users.noreply.github.com>
2024-05-17 20:31:19 +0500

    changes

--- asyncProgrammigQuestoin.dart
+++ asyncProgrammigQuestoin.dart
@@ -1,1 +1,8 @@
-void main() {}
+// async programming helps in performing multipule task like fetch data form the internet & etc
+//Future in dart help to delya any tak that you want to executer in future for some reason and helps in asyncroinsation in your code
+
+void main() {
+  var value = Future.delayed(Duration(seconds: 2), () => 'hello wrold');
+  value.then((value) => print(value));
+  
+}
