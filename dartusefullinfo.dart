5ed53ba45b4b1529cc33b01a6c2aef7fe1b33ed2
Saad ur Rehmah <125679304+saadrehman10@users.noreply.github.com>
2024-05-20 11:31:50 +0500

    changes

--- dartusefullinfo.dart
+++ dartusefullinfo.dart
@@ -28,4 +28,15 @@ void main() {
   print("Year is " + myBirthday.year.toString());
   print("Month is " + myBirthday.month.toString());
   print("Day is " + myBirthday.day.toString());
+  print('=======');
+ 
+   DateTime myBirthday2 = DateTime.parse("1997-05-14");
+  DateTime today = DateTime.now();
+  Duration diff = today.difference(myBirthday2);
+  print("Difference in days: " + diff.inDays.toString());
+  print("Difference in hours: " + diff.inHours.toString());
+  print("Difference in minutes: " + diff.inMinutes.toString());
+  print("Difference in seconds: " + diff.inSeconds.toString());
+  print("Difference in milliseconds: " + diff.inMilliseconds.toString());
+  print("Difference in microseconds: " + diff.inMicroseconds.toString());
 }
