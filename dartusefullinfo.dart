ce62d492b491bf8622f718fb17b3fdc7ca92a9ef
Saad ur Rehmah <125679304+saadrehman10@users.noreply.github.com>
2024-05-20 11:24:23 +0500

    changes

--- dartusefullinfo.dart
+++ dartusefullinfo.dart
@@ -16,4 +16,11 @@ void main() {
   print(date.runtimeType);
   Person obj = new Person('Saad');
   print(obj.getvalue);
+
+
+  String myDateInString = "2022-05-01";
+  DateTime myConvertedDate = DateTime.parse(myDateInString);
+  print("Year is " + myConvertedDate.year.toString());
+  print("Month is " + myConvertedDate.month.toString());
+  print("Day is " + myConvertedDate.day.toString());
 }
