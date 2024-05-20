6fd97653b13204902ac0471776840060aaa78e77
Saad ur Rehmah <125679304+saadrehman10@users.noreply.github.com>
2024-05-20 11:16:23 +0500

    changes

--- dartusefullinfo.dart
+++ dartusefullinfo.dart
@@ -1,4 +1,17 @@
+import 'dart:io';
+
+class Person {
+  final String? name;
+  static const int? value = null;
+  Person(
+    this.name,
+  );
+  set setvaue(props) {value = props}
+}
+
 void main() {
   final date = DateTime.now();
-  print(date);
+  print(date.runtimeType);
+  var date2 = DateTime.now();
+  print(date.runtimeType);
 }
