d3864e4bf46b0196b15eaf73b04c2c3a8db5a559
Saad ur Rehmah <125679304+saadrehman10@users.noreply.github.com>
2024-05-21 15:18:23 +0500

    changes

--- Extention.dart
+++ Extention.dart
@@ -1,8 +1,10 @@
 void main() {
   List<int> array = [1, 25, 2, 1, 5, 3, 5, 2, 34];
-  print(array.sum);
+  print(array.sum(3));
 }
 
 extension on List<int> {
-  int get sum => fold(0, (a, b) => a + b);
+  int sum(int value) {
+    return fold(value, (a, b) => a + b);
+  }
 }
