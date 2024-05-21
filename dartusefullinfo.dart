600c4e272140a83f92e37e47d0fe7c37f229c345
Saad ur Rehmah <125679304+saadrehman10@users.noreply.github.com>
2024-05-21 14:32:01 +0500

    changes

--- dartusefullinfo.dart
+++ dartusefullinfo.dart
@@ -10,37 +10,46 @@ class Person {
 }
 
 void main() {
-  final date = DateTime.now();
-  print(date);
-  var date2 = DateTime.now();
-  print(date2.runtimeType);
-  Person obj = new Person('Saad');
-  print(obj.getvalue);
+  // final date = DateTime.now();
+  // print(date);
+  // var date2 = DateTime.now();
+  // print(date2.runtimeType);
+  // Person obj = new Person('Saad');
+  // print(obj.getvalue);
 
-  String myDateInString = "2022-05-01";
-  DateTime myConvertedDate = DateTime.parse(myDateInString);
-  print("Year is " + myConvertedDate.year.toString());
-  print("Month is " + myConvertedDate.month.toString());
-  print("Day is " + myConvertedDate.day.toString());
-  print('=======');
-  DateTime myBirthday = DateTime.parse("1997-05-14");
-  myBirthday = myBirthday.add(Duration(days: 1));
-  print("Year is " + myBirthday.year.toString());
-  print("Month is " + myBirthday.month.toString());
-  print("Day is " + myBirthday.day.toString());
-  print('=======');
- 
-   DateTime myBirthday2 = DateTime.parse("1997-05-14");
-  DateTime today = DateTime.now();
-  Duration diff = today.difference(myBirthday2);
-  print("Difference in days: " + diff.inDays.toString());
-  print("Difference in hours: " + diff.inHours.toString());
-  print("Difference in minutes: " + diff.inMinutes.toString());
-  print("Difference in seconds: " + diff.inSeconds.toString());
-  print("Difference in milliseconds: " + diff.inMilliseconds.toString());
-  print("Difference in microseconds: " + diff.inMicroseconds.toString());
-  
+  // String myDateInString = "2022-05-01";
+  // DateTime myConvertedDate = DateTime.parse(myDateInString);
+  // print("Year is " + myConvertedDate.year.toString());
+  // print("Month is " + myConvertedDate.month.toString());
+  // print("Day is " + myConvertedDate.day.toString());
+  // print('=======');
+  // DateTime myBirthday = DateTime.parse("1997-05-14");
+  // myBirthday = myBirthday.add(Duration(days: 1));
+  // print("Year is " + myBirthday.year.toString());
+  // print("Month is " + myBirthday.month.toString());
+  // print("Day is " + myBirthday.day.toString());
+  // print('=======');
 
+  //  DateTime myBirthday2 = DateTime.parse("1997-05-14");
+  // DateTime today = DateTime.now();
+  // Duration diff = today.difference(myBirthday2);
+  // print("Difference in days: " + diff.inDays.toString());
+  // print("Difference in hours: " + diff.inHours.toString());
+  // print("Difference in minutes: " + diff.inMinutes.toString());
+  // print("Difference in seconds: " + diff.inSeconds.toString());
+  // print("Difference in milliseconds: " + diff.inMilliseconds.toString());
+  // print("Difference in microseconds: " + diff.inMicroseconds.toString());
+
+  var date = DateTime.now();
+  print(date.year);
+  print(date.second);
+  print(date.microsecond);
+  print(date.day);
+  print(date.hour);
+  print(date.minute);
+  print(date.timeZoneName);
+  print(date.weekday);
+  print(date.timeZoneOffset);
 
 
 }
