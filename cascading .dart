a2774afc8db5dcf3b7f41aaf441592c4f6c14071
Saad ur Rehmah <125679304+saadrehman10@users.noreply.github.com>
2024-05-22 19:52:01 +0500

    update

--- cascading .dart
+++ cascading .dart
@@ -21,23 +21,26 @@ extension on Random<int> {
 }
 
 void main() {
-  List<int> array = [1, 2, 54, 3];
-  print(array);
-  array
-    ..add(2)
-    ..add(30)
-    ..insert(3, 22)
-    ..addAll([
-      2,
-      3,
-      342,
-    ])
-    ..insertAll(5, [23232, 2322])
-    ..forEach((element) {
-      print(element);
-    })
-    ..asMap();
+  // List<int> array = [1, 2, 54, 3];
+  // print(array);
+  // array
+  //   ..add(2)
+  //   ..add(30)
+  //   ..insert(3, 22)
+  //   ..addAll([
+  //     2,
+  //     3,
+  //     342,
+  //   ])
+  //   ..insertAll(5, [23232, 2322])
+  //   ..forEach((element) {
+  //     print(element);
+  //   })
+  //   ..asMap();
 
   Random<int> obj = Random<int>(22);
+  Random<String> obj2 = Random<String>('hello');
   print(obj.sum1(22));
+  print(obj2 ..dispalyRandom() ..returnjust('hel'));
+
 }
