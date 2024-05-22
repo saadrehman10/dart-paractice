d8f857b43f172ae4ca6d0157554e2195ed11caf6
Saad ur Rehmah <125679304+saadrehman10@users.noreply.github.com>
2024-05-22 19:17:58 +0500

    changes

--- cascading .dart
+++ cascading .dart
@@ -1,1 +1,18 @@
-void main() {}
+void main() {
+  List<int> array = [1, 2, 54, 3];
+  print(array);
+  array
+    ..add(2)
+    ..add(30)
+    ..insert(3, 22)
+    ..addAll([
+      2,
+      3,
+      342,
+    ])
+    ..insertAll(5, [23232, 2322])
+    ..forEach((element) {
+      print(element);
+    })
+    ..asMap();
+}
